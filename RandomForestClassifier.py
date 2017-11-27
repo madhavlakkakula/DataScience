942eb432b5060af16e5a529bbc2bf5e87b5fe0ca
Madhav Lakkakula <33281922+madhavlakkakula@users.noreply.github.com>
2017-11-27 12:03:42 +0530

    Update RandomForestClassifier.py

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -20,8 +20,6 @@ titanic_train.shape
 titanic_train.info()
 
 titanic_test = pd.read_csv('test.csv')
-titanic_test.shape
-titanic_test.info()
 titanic_test.Survived = None
 
 #it gives the same never of levels for all the categorical variables
@@ -31,6 +29,11 @@ titanic = pd.concat([titanic_train, titanic_test])
 def extract_title(name):
      return name.split(',')[1].split('.')[0].strip()
 titanic['Title'] = titanic['Name'].map(extract_title)
+titanic.Title.unique()
+
+titanic['Title'] = titanic['Title'].replace(to_replace=['Mme','the Countess'],value='Mrs')
+titanic['Title'] = titanic['Title'].replace(to_replace=['Ms','Mlle'],value='Miss')
+
 
 #create an instance of Imputer class with required arguments
 mean_imputer = preprocessing.Imputer()
@@ -65,30 +68,23 @@ titanic['FamilySize1'] = titanic['FamilySize'].map(convert_familysize)
 
 #convert categorical columns to one-hot encoded columns
 titanic1 = pd.get_dummies(titanic, columns=['Sex','Pclass','Embarked', 'Age1', 'Title', 'FamilySize1'])
-titanic1.shape
-titanic1.info()
 
-titanic2 = titanic1.drop(['PassengerId','Name','Age','Ticket','Cabin','Survived'], axis=1, inplace=False)
-titanic2.shape
+titanic2 = titanic1.drop(['PassengerId','Name','Age','Ticket','SibSp','Parch','Cabin','FamilySize','Survived','Title_Dona','Title_Don','Title_Dr','Title_Lady','Title_Jonkheer'], axis=1, inplace=False)
 
 X_train = titanic2[0:titanic_train.shape[0]]
-X_train.shape
-X_train.info()
 y_train = titanic_train['Survived']
 
 #oob scrore is computed as part of model construction process
-rf_estimator = ensemble.RandomForestClassifier(oob_score=True, random_state=2017)
+rf_estimator = ensemble.RandomForestClassifier(oob_score=False, random_state=2017)
 #n_estimators: No. of trees to be grown
 #max_features: Maximum no. of fetures/columns to be used in each tree
 #Max_depth: Same old... Maximum depth of each tree to grow
-rf_grid = {'n_estimators':[3], 'max_features':[5,6,7], 'max_depth':[3,4,5]}
+rf_grid = {'n_estimators':[100],'max_features':[5], 'max_depth':[3,4,5,6,7],'verbose':[1]}
 grid_rf_estimator = model_selection.GridSearchCV(rf_estimator, rf_grid, cv=10,n_jobs=1)
 grid_rf_estimator.fit(X_train, y_train)
-print(grid_rf_estimator.grid_scores_)
 print(grid_rf_estimator.best_score_)
-print(grid_rf_estimator.best_params_)
 #oob_score_ is to calulcate the accuracy like CV score. Since we used oob_score=True, we have to calculate oob_score_
-grid_rf_estimator.best_estimator_.oob_score_
+#grid_rf_estimator.best_estimator_.oob_score_
 #Train score
 print(grid_rf_estimator.score(X_train, y_train))
 
@@ -98,8 +94,8 @@ importances = grid_rf_estimator.best_estimator_.feature_importances_
 fe_df = pd.DataFrame({'feature':features, 'importance': importances})
 
 X_test = titanic2[titanic_train.shape[0]:]
-X_test.shape
-X_test.info()
+#X_test.shape
+#X_test.info()
 titanic_test['Survived'] = grid_rf_estimator.predict(X_test)
 
-titanic_test.to_csv('submission.csv', columns=['PassengerId','Survived'],index=False)
+titanic_test.to_csv('submission_rf.csv', columns=['PassengerId','Survived'],index=False)
